5a20f448b9dec740d4c7fa253b92fceec1b20fd0
SvitLime <150146888+SvitLime@users.noreply.github.com>
2024-03-06 13:55:33 +0200

    Update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,94 +1,31 @@
-// const mobileMenu = document.querySelector('.mobile-menu');
-// const menuBtnOpen = document.querySelector('.menu-btn-open');
-// const menuBtnClose = document.querySelector('.menu-btn-close');
+const dropButtons = document.querySelectorAll('.dropbtn');
+const dropMenus = document.querySelectorAll('.myDropdown');
+const overlay = document.querySelector('.overlay')
 
-// const toggleMenu = () => mobileMenu.classList.toggle('is-open');
-
-// menuBtnOpen.addEventListener('click', toggleMenu);
-// menuBtnClose.addEventListener('click', toggleMenu);
-
-
-/////////////
-
-
-// function myFunction() {
-
-//   document.querySelectorAll('.myDropdown').classList.toggle("show");
-  
-
-//   // document.getElementById("myDropdown").classList.toggle("show");
-//   // document.getElementById("myMain").classList.toggle("hide");
-// }
-
-// Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//       }
-//     var main = document.getElementsByClassName("myMain");
-//     for (i = 0; i < main.length; i++) {
-//       var openDropdownMain = main[i];
-//       if (openDropdownMain.classList.contains('hide')) {
-//         openDropdownMain.classList.remove('hide');
-//       }
-//     }
-//   }
-// }
-
-
-// function toggleDropdown(btnId) {
-//   var dropdown = document.getElementById(btnId).nextElementSibling;
-//   dropdown.classList.toggle("show");
-// }
-
-// // Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     for (var i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
-
-
-// Get all elements with class "dropbtn"
-var dropButtons = document.querySelectorAll('.dropbtn');
-
-// Add click event listeners to each button
-dropButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    // Toggle the "close" class for the clicked button
-    this.classList.toggle('close');
-  });
-});
-
-////////////////////////
-
-// var dropButtons = document.querySelectorAll('.dropbtn');
-// var dropMain = document.querySelectorAll('.myMain');
-
-dropButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
-    // Get the sibling element (dropdown) of the clicked button
-    var dropdown = this.nextElementSibling;
-
-    // Check if the dropdown exists
-    if (dropdown) {
-      // Toggle the "show" class for the associated dropdown
-      dropdown.classList.toggle('show');
-
+dropButtons.forEach(function (button) {
+  const dropMenu = button.nextElementSibling
+  button.addEventListener('click', () => {
+    if (!button.classList.contains('close')) {
+      button.classList.add('close');
+      dropMenu.classList.add('show');
+      overlay.style.display = 'block';
+      document.body.style.overflow = 'hidden'
+    } else {
+      button.classList.remove('close');
+      dropMenu.classList.remove('show')
+      overlay.style.display = 'none';
+      document.body.style.overflow = ''
     }
-
-  });
+  })
 });
 
+overlay.addEventListener('click', () => {
+  dropButtons.forEach(button => {
+    button.classList.remove('close')
+  })
+  dropMenus.forEach(menu => {
+    menu.classList.remove('show')
+  })
+  overlay.style.display = 'none';
+  document.body.style.overflow = '';
+})
